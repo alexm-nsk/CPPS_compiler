75f97e6f7dc977f589d0982d51278fbe5752fc3c
alexm-nsk <alex.m.work@yandex.ru>
2021-07-08 17:30:41 +0700

    intermediate

--- sample/exporters/json.py
+++ sample/exporters/json.py
@@ -246,6 +246,7 @@ def export_if_to_json(node, parent_node):
 
     # process the condition:____________________________________________________________________________
     
+    #                             TODO  ↓       cond has to be boolean
     inPorts, outPorts = genPorts(["integer"], ["boolean"], node.condition[0].node_id)
     
     ret_val["condition"] = dict(outPorts = outPorts, inPorts = inPorts )
