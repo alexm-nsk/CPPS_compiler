b17d844b86198451acc7fc976c654cfec87abbf8
alexm-nsk <alex.m.work@yandex.ru>
2021-07-29 10:49:27 +0700

    intermediate

---
+++ sample/ast_/port.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  port.py
+#
+#  Copyright 2021 alexm
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+from sisal_type.sisal_type import *
+
+ #"inPorts": [
+ #   {
+ #    "nodeId": "node3",
+ #    "type": {
+ #     "location": "1:14-1:15",
+ #     "element": {
+ #      "location": "1:25-1:29",
+ #      "name": "real"
+ #     }
+ #    },
+ #    "index": 0
+ #   }
+ #  ],
+ #  "outPorts": [
+ #   {
+ #    "nodeId": "node3",
+ #    "type": {
+ #     "location": "1:38-1:42",
+ #     "name": "real"
+ #    },
+ #    "index": 0
+ #   }
+ #  ],
+
+
+class Port:
+    def __init__(self, node_id, type_, index):
+        self.node_id = node_id
+        self.type = type_
+        self.index = index
+
+    def emit_json():
+        return dict(nodeId = self.node_id,
+                    type   = self.type_.emit_json(),
+                    index  = self.index)
